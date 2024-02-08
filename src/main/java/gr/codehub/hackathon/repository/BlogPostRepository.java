843f3c97d5e22299aa21e3bd93653b7285f41a95
Thomas Varsamidis <thomastuesday23899.2@gmail.gmail>
2024-02-08 18:52:49 +0200

    Correct Repository functionality

--- src/main/java/gr/codehub/hackathon/repository/BlogPostRepository.java
+++ src/main/java/gr/codehub/hackathon/repository/BlogPostRepository.java
@@ -1,6 +1,7 @@
 package gr.codehub.hackathon.repository;
 
-import gr.codehub.hackathon.domain.BlogPost;
+
+import gr.codehub.hackathon.model.BlogPost;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- src/main/java/gr/codehub/hackathon/repository/CommentRepository.java
+++ src/main/java/gr/codehub/hackathon/repository/CommentRepository.java
@@ -1,6 +1,7 @@
 package gr.codehub.hackathon.repository;
 
-import gr.codehub.hackathon.domain.Comment;
+
+import gr.codehub.hackathon.model.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
